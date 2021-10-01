e43681c62d5b5f4dc939c47c8063934f6985b682
emrgncr <egencer20@ku.edu.tr>
2021-10-01 13:25:50 +0000

    tekrar deneme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
+import numpy as np
 x = input("enter number x: ")
 y = input("enter number y: ")
+x = int(x)
+y = int(y)
 print(f"x**y = {x**y}")
-print(f"log(x) = {math.log(x,2)}")
+print(f"log(x) = {np.log2(x)}")
 print(76162)
