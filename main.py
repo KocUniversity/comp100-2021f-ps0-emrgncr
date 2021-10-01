baeab728a6f0c61a0b3d382f10e28003d5e1a38a
emrgncr <egencer20@ku.edu.tr>
2021-10-01 16:32:49 +0300

    vsdeneme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
-x = input("enter number x: ")
-y = input("enter number y: ")
-x = int(x)
-y = int(y)
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
 print(f"x**y = {x**y}")
 print(f"log(x) = {np.log2(x)}")
 print(76162)
